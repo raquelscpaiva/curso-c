2ee555dc33e172034ea380c458571f81179816f4
raquelscpaiva <raquel.siqueirac@gmail.com>
2020-12-30 13:21:16 -0300

    exercícios aula 2

---
+++ aula2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    //   esse eh um comentario de uma linha
+    /*   comentario de 
+         multiplas linhas
+    */
+
+    int idade = 20, ano = 2016;
+
+    float pi = 3.14;
+    
+    printf("Idade: %d\n", idade);
+    printf("Ano: %d\n", ano);
+    printf("PI: %.2f\n", pi);
+
+    return 0;
+}
